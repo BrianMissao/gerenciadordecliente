29bce0578be126d0083157763ebd986254ea53e4
Brian Missao <brianmissao@gmail.com>
2024-05-04 08:52:34 -0400

    primeiro commit

---
+++ src/main/java/com/teste/gerenciadordeusuario/GerenciadordeusuarioApplication.java
@@ -0,0 +1,13 @@
+package com.teste.gerenciadordeusuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadordeusuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadordeusuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teste/gerenciadordeusuario/GerenciadordeusuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.gerenciadordeusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadordeusuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
